34eef2dd7ba69a388f444b29a00285add53f6929
Gonzalo Diez <gonzalodiez97@gmail.com>
2021-09-11 16:25:02 -0300

    actualización1

--- js/general.js
+++ js/general.js
@@ -1,14 +1,22 @@
-//Ajuste del menú para celulares
-
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    menu.classList.remove('active'); 
-}
+//Barra buscadora
+let suggestions = [
+    "Nike",
+    "Adidas",
+    "Zapatillas Nike",
+    "Zapatillas Adidas",
+    "Pelotas de Futbol",
+    "Pelotas de Basquet",
+    "Pelotas de Volley",
+    "Pelotas de Tenis",
+    "Pelotas de Ping Pong",
+    "Camisetas de Argentina",
+    "Camisetas de Boca",
+    "Camisetas de River",
+    "Camisetas de San Lorenzo",
+    "Camisetas de Racing",
+    "Camisetas de Huracan",
+    "Camisetas de Barcelona",
+    "Camisetas de Paris Saint Germain",
+    "Camisetas de Atletico Madrid",
+    "Camisetas de Real Madrid",
+];
---
+++ js/script.js
@@ -0,0 +1,72 @@
+//Ajuste del menú para celulares
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    menu.classList.remove('active'); 
+}
+
+// getting all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+// if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            webLink = `https://www.google.com/search?q=${userData}`;
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = `<li>${data}</li>`;
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
+}
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = `https://www.google.com/search?q=${selectData}`;
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
+
