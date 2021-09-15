26f52f5eaf1f6431ec8ed9c13be5ea0f32db1054
Gonzalo Diez <gonzalodiez97@gmail.com>
2021-09-15 14:19:35 -0300

    barra busqueda final

--- js/index.js
+++ js/index.js
@@ -59,6 +59,61 @@ document.querySelectorAll('.imagen-destacada-3').forEach(image_3 =>{
   });
 });  
 
+// obteniendo todos los elementos requeridos
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+// si el usuario presiona cualquier tecla y suelta
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //usuario entro los datos
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtrar el valor del array y los caracteres del usuario a minúsculas y devolver solo aquellas palabras que comienzan con caracteres ingresados ​​por el usuario
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            //pasar datos de retorno dentro de la etiqueta li
+            return data = `<li>${data}</li>`;
+        });
+        searchWrapper.classList.add("active"); //muestra autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //agregando atributo onclick en todas las etiqutas li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //esconde autocomplete box
+    }
+}
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
+
 class mercancia {
   constructor(producto, cantidad, precio){
     this.producto = producto;
