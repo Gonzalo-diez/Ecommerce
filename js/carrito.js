c90bfc827411551279106f31290e716741717d92
Gonzalo Diez <gonzalodiez97@gmail.com>
2021-09-18 17:35:03 -0300

    cambios

---
+++ js/carrito.js
@@ -0,0 +1,48 @@
+let baseDatos = [{
+    Id: 0,
+    producto: "Zapatillas Nike",
+    Cantidad: 50,
+    precio: 15000,
+},
+{
+    Id: 1,
+    producto: "Zapatillas Nike",
+    Cantidad: 75,
+    precio: 13000,
+},
+{
+    Id: 2,
+    producto: "Zapatillas Nike",
+    Cantidad: 75,
+    precio: 16000,
+},
+{
+    Id: 3,
+    producto: "Zapatillas Nike",
+    Cantidad: 75,
+    precio: 15800,
+},
+{
+    Id: 4,
+    producto: "Zapatillas Nike",
+    Cantidad: 75,
+    precio: 17000,
+},
+{
+    Id: 5,
+    producto: "Zapatillas Nike",
+    Cantidad: 75,
+    precio: 14500,
+}];
+
+function carrito() {
+
+}
+
+function agregarCarrito() {
+
+}
+
+function removerCarrito() {
+
+}
