43a218b67a873e39e1a771efc67df307b5cc0dfe
Gonzalo Diez <gonzalodiez97@gmail.com>
2021-09-18 23:50:46 -0300

    arreglos

--- js/carrito.js
+++ js/carrito.js
@@ -1,53 +1,41 @@
-let producto1 = {
+let productos = [{
     Id: 0,
     producto: "Zapatillas Nike",
     Cantidad: 50,
     precio: 15000,
-};
-
-let producto2 = {
+},
+{
     Id: 1,
     producto: "Zapatillas Nike",
     Cantidad: 75,
     precio: 13000,
-};
-
-let producto3 = {
+},
+{
     Id: 2,
     producto: "Zapatillas Nike",
     Cantidad: 75,
     precio: 16000,
-};
-
-let producto4 = {
+},
+{
     Id: 3,
     producto: "Zapatillas Nike",
     Cantidad: 75,
     precio: 15800,
-};
-
-let producto5 = {
+},
+{
     Id: 4,
     producto: "Zapatillas Nike",
     Cantidad: 75,
     precio: 17000,
-};
-
-let producto6 = {
+},
+{
     Id: 5,
     producto: "Zapatillas Nike",
     Cantidad: 75,
     precio: 14500,
-};
+}]
 
 function carrito() {
-
-}
-
-function agregarCarrito() {
-    
-}
-
-function removerCarrito() {
-
+    let baseDatos = productos;
+    console.log(baseDatos);
 }
